7b8246d323e5904a6700916ba1bab2f1fe686148
Camden <176319147+cmwestfall@users.noreply.github.com>
2024-11-04 14:40:29 -0500

    final commit

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-function accumulateTimes(sampleArr) {
+function timeAccumulator(plants) {
   let output = {};
 
-  for (let plant of sampleArr) {
-    output[plant.id] = {};
+  for (let plant of plants) {
+    output[plant.id] = {}; // output = {id, {}}
     let runningTotal = 0;
 
     for (let step of plant.productionSteps) {
       runningTotal += step.time;
-      output[plant.id][step.step] = runningTotal;
+      output[plant.id][step.step] = runningTotal; // object[object[0]][object[0][0]] = object[0][1]
     }
   }
   console.log(output);
 }
 
-accumulateTimes([
+timeAccumulator([
   {
     id: "Plant1",
     productionSteps: [
